f19d082cacd7e4058df5368553b8fb34c8266793
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-12-21 21:46:51 +0530

    gg

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import { motion } from "framer-motion";
 
 const AboutPage = () => {
